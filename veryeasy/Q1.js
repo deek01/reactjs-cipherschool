55528dd5daecdbfd8f1e75d00f4434c0b4ce8edb
deek01 <40050951+deek01@users.noreply.github.com>
2020-08-13 23:53:25 +0530

    Add files via upload

---
+++ veryeasy/Q1.js
@@ -0,0 +1,8 @@
+function time_convert(num)
+ { 
+  var seconds= num*60;  
+  return seconds;         
+}
+console.log(time_convert(5));
+console.log(time_convert(3));
+console.log(time_convert(2));
---
+++ veryeasy/Q2.js
@@ -0,0 +1,14 @@
+function check_even(a,b)
+{
+    
+    if(a%b==0)
+    {
+    greeting="True";
+    }
+    else
+    {
+    greeting ="False";
+    }
+    return greeting;
+}
+console.log(check_even(98,7));
